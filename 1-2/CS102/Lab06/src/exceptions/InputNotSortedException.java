ad6a47d33948020c76f2b4905bf459b992b21b14
Ahmet Berke Gökmen <ahmet.gokmen@getir.com>
2023-04-07 23:31:26 +0300

    submodule repo

--- 1-2/CS102/Lab06/src/exceptions/InputNotSortedException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class InputNotSortedException extends Exception {
-
-  public InputNotSortedException() {
-    super("Input array must be sorted");
-  }
-}
--- 1-2/CS102/Lab06/src/exceptions/KIsLessThan2Exception.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class KIsLessThan2Exception extends Exception {
-
-  public KIsLessThan2Exception() {
-    super("k cannot be less than 2");
-  }
-}
